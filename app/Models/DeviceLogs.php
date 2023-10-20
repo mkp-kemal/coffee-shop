320acab67d4e0ab1384d382c43e366f362e1c298
faisalach <faisalachramsyah@gmail.com>
2023-10-20 21:03:50 +0700

    create model

---
+++ app/Models/DeviceLogs.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DeviceLogs extends Model
+{
+    use HasFactory;
+    protected $primaryKey = "id_device_logs";
+}
