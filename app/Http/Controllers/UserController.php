923e91628dea48245ed3d1c9c21c246d674fa9e1
faisalach <faisalachramsyah@gmail.com>
2023-12-03 19:12:54 +0700

    check role

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,36 +4,42 @@ namespace App\Http\Controllers;
 
 use App\Models\Users;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function get_all(Request $request){
+        $user       = Auth::user();
+        if($user->role !== "admin"){
+            return response(["unauthorized"],401);
+        }
+
         $limit      = $request->input("limit");
         $offset     = $request->input("offset");
         $keyword    = $request->input("search");
         $order      = $request->input("order");
 
-        $user = Users::select("username","role");
+        $data = Users::select("username","role");
 
         if(!empty($keyword)){
-            $user->where(function($query) use ($keyword) {
+            $data->where(function($query) use ($keyword) {
                 $query->where("username","like","%$keyword%");
                 $query->orWhere("role","like","%$keyword%");
             });
         }
 
-        $total_data    = $user->count();
+        $total_data    = $data->count();
 
         if(!empty($order)){
-            $user->orderBy($order[0],$order[1]);
+            $data->orderBy($order[0],$order[1]);
         }
 
-        $user->limit($limit);
-        $user->offset($offset);
+        $data->limit($limit);
+        $data->offset($offset);
 
-        return [
-            "data"      => $user->get(),
+        return response([
+            "data"      => $data->get(),
             "totalData" => $total_data,
-        ];
+        ]);
     }
 }
